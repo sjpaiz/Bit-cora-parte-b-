afd76eeb4bcd33abe4f70a55cc3d7e55c58b05ee
sjpaiz <158302451+sjpaiz@users.noreply.github.com>
2024-05-15 08:57:21 -0600

    Add files via upload

---
+++ L7 - Sofia Paiz 1092224/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Program 
+{
+    static void Main (string[] args)
+    {
+        Console.WriteLine("Laboratorio 07 - Sofia Paiz - 1092224");
+        int NFibonacci;
+        bool conversion = false;
+        bool nPositivo = true;
+
+        do
+        {
+            Console.WriteLine("Ingrese un número mayor a 0");
+            conversion = int.TryParse(Console.ReadLine(), out NFibonacci);
+            if (conversion)
+            {
+                if (NFibonacci > 0)
+                {
+                    nPositivo = true;
+                }
+                else
+                {
+                    nPositivo = false;
+                    Console.WriteLine("Debe ingresar un número mayor a 0.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Debe ingresar un número válido.");
+            }
+        }
+        while (!conversion || !nPositivo);
+
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        int i = 0;
+        string resultado = "";
+
+        if (NFibonacci > 0)
+        {
+            resultado = a + ",";
+        }
+
+        if (NFibonacci > 1)
+        {
+            resultado += b + ",";
+        }
+
+        while (i < NFibonacci - 2) 
+        {
+            c = a + b; 
+            resultado += c + ",";
+            a = b;
+            b = c;
+            i++;
+        }
+
+        Console.WriteLine($"La secuencia de Fibonacci hasta el número {NFibonacci} es: {resultado}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ LAB 08 - SOFIA PAIZ 1092224/LAB 08 - SOFIA PAIZ 1092224/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Data;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int opcion = 0;
+        string entrada;
+
+        do
+        {
+            Console.WriteLine("Menú");
+            Console.WriteLine(" 1) sumatoria");
+            Console.WriteLine(" 2) factorial");
+            Console.WriteLine(" 3) tablas");
+            Console.WriteLine(" 1) salida");
+            Console.WriteLine("Ingrese un número 1-4 ");
+            entrada = Console.ReadLine();
+            try
+            {
+                opcion = int.Parse(entrada);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("error");
+            }
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Ingrese un número ");
+                    string entradanumero = Console.ReadLine();
+                    int n = 0;
+                    try
+                    {
+                        n = int.Parse(entradanumero);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Error, ingrese otro número");
+                    }
+
+                    int i=1;
+                    int sumatoria=0;
+                    while(i<=n)
+                    {
+                        sumatoria += i;
+                        i++;
+                    }
+
+                    Console.WriteLine($"Sumatoria: {sumatoria}");
+                    
+                    break;
+
+                case 2:
+                    Console.WriteLine("Ingrese un número ");
+                    string entradanumerof = Console.ReadLine();
+                    int nf = 0;
+                    try
+                    {
+                        nf = int.Parse(entradanumerof);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Error, ingrese otro número");
+                    }
+
+                    int j=1;
+                    int factorial=1;
+                    while(j<=nf)
+                    {
+                         factorial *= j;
+                        j++;
+                    }
+
+                    Console.WriteLine($"Factorial: {factorial}");
+                    
+                    break;
+    
+
+                case 3:
+                string titulo = "\t" ;
+                for (int ititulo = 1; ititulo <= 10; ititulo++)
+                {
+                    titulo += ititulo + "\t";
+                }
+                Console.WriteLine(titulo);
+
+                string fila = "";
+                for (int ifila = 1; ifila <= 10; ifila++)
+                {
+                    fila = ifila + "\t";
+                    for (int multiplo = 1; multiplo <= 10; multiplo++)
+                    {
+                        fila+= ifila * multiplo + "\t";
+                    }
+                    Console.WriteLine(fila);
+
+                }
+                break;
+
+                default:
+                    Console.WriteLine("Error, ingrese un número del 1 al 4");
+                    break;
+            }
+        }
+        while (opcion != 4);
+    }
+}
+
+class CuentaBancaria
+{
+ 
+    string titular;
+    double saldo;
+
+   
+    public CuentaBancaria(string titular, double saldoInicial)
+    {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public void Acreditar(double monto)
+    {
+        saldo += monto;
+        Console.WriteLine($"Se acreditó {monto:C}. Nuevo saldo: {saldo:C}");
+    }
+
+    public void Debitar(double monto)
+    {
+        if (monto > saldo)
+        {
+            Console.WriteLine("Saldo insuficiente.");
+        }
+        else
+        {
+            saldo -= monto;
+            Console.WriteLine($"Se debitó {monto:C}. Nuevo saldo: {saldo:C}");
+        }
+    }
+    public void MostrarSaldo()
+    {
+        Console.WriteLine($"Saldo actual: {saldo:C}");
+    }
+}
